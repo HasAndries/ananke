00c149a9041670e2beadd29e0630b04528ae908e
HasAndries <hasandries@I4-DEV.(none)>
2011-02-22 17:43:52 +0200

    Get Object Id outputs warning instead of error if key not found

--- spec/lib/route_for_spec.rb
+++ spec/lib/route_for_spec.rb
@@ -10,7 +10,7 @@ describe 'Resource Route-For' do
   end
 
   it """
-  Should be able to accept route_for Registrations
+  should be able to accept route_for Registrations
   """ do
     module Repository
       module Route_for
@@ -30,7 +30,7 @@ describe 'Resource Route-For' do
   end
 
   it """
-  Should be able to accept route_for Registrations
+  should be able to accept route_for Registrations with POST and multiple input parameters
   """ do
     module Repository
       module Route_for
@@ -47,5 +47,51 @@ describe 'Resource Route-For' do
     post "/route_for/multi", body={:id => 1, :name => 'some name'}
     check_status(200)
     last_response.body.should == '{"route_for_list":[{"route_for":{"route_for_id":"1","content":"Test"},"links":[{"rel":"self","uri":"/route_for/1"}]}],"links":[{"rel":"self","uri":"/route_for/multi"}]}'
+  end
+
+  it """
+  should be able to register multiple route_for's in one declaration
+  """ do
+    module Repository
+      module Route_for
+        def self.get_1(id)
+        end
+        def self.get_2(id)
+        end
+      end
+    end
+    route :route_for do
+      id :route_for_id
+      route_for :get_1
+      route_for :get_2
+    end
+
+    get "/route_for/get_1/1"
+    check_status(200)
+    get "/route_for/get_2/2"
+    check_status(200)
+  end
+
+  it """
+  should be able to register multiple route_for's with POST in one declaration
+  """ do
+    module Repository
+      module Route_for
+        def self.post_1(id)
+        end
+        def self.post_2(id)
+        end
+      end
+    end
+    route :route_for do
+      id :route_for_id
+      route_for :post_1, :post
+      route_for :post_2, :post
+    end
+
+    post "/route_for/post_1/1"
+    check_status(200)
+    post "/route_for/post_2/2"
+    check_status(200)
   end
 end
