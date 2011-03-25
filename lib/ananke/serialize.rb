63047b3b8759a65873337d6a222ebab2c05eed11
Andries Coetzee <andriesc@mixtel.com>
2011-03-25 10:51:02 +0200

    Serialization removes empty values from return objects with the toggle of remove_empty setting

--- lib/ananke/serialize.rb
+++ lib/ananke/serialize.rb
@@ -10,20 +10,26 @@ module Serialize
 
     if obj.class == Hash
       obj.each do |k, v|
-        ret[k.to_sym] = (can_serialize?(v) ? v : Serialize.to_h(v))
+        if !v.nil? and v != ''
+          ret[k.to_sym] = (can_serialize?(v) ? v : Serialize.to_h(v))
+        end
       end
     elsif obj.class == Array
       ret = []
       obj.each do |i|
-        #ret << (can_serialize?(i) ? i : Serialize.to_hash(i))
-        ret << Serialize.to_h(i)
+        if !i.nil? and i != ''
+          #ret << (can_serialize?(i) ? i : Serialize.to_hash(i))
+          ret << Serialize.to_h(i)
+        end
       end
     elsif obj.instance_variables.empty?
       ret = obj
     else
       obj.instance_variables.each do |e|
         value = obj.instance_variable_get e.to_sym
-        ret[e[1..-1]] = (can_serialize?(value) ? value : Serialize.to_h(value))
+        if !value.nil? and value != ''
+          ret[e[1..-1]] = (can_serialize?(value) ? value : Serialize.to_h(value))
+        end
       end
     end
     ret
