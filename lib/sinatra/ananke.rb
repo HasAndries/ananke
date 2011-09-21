7c9148b47252403c5a004a4ac4afc81a46b7d0dd
Armand Fourie <arrie@U500.(none)>
2011-09-21 16:52:42 +0200

    added the route checking back in

--- lib/sinatra/ananke.rb
+++ lib/sinatra/ananke.rb
@@ -128,8 +128,9 @@ module Sinatra
       }
       id_param = options.delete :id
       block_params = block.parameters.collect {|p| p[1]}
-      path = "#{path}/:#{block_params[0]}" if (block_params.length == 1 && path != ":#{block_params[0]}") || id_param
-
+      if [:get,:put,:delete].include?(type)
+        path = "#{path}/:#{block_params[0]}" if (block_params.length == 1 && path != ":#{block_params[0]}") || id_param
+      end
       method(type).call "/#{resource_name}/#{path}", options, do
         inject_app(res[:classes])
         input_params = collect_input_params(params, &block)
